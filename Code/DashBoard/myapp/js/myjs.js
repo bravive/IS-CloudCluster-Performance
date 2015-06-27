e035716d17f5534381f9854f52ea6ba158a4ca5c
Mengye Gong <gongmengye@gmail.com>
2015-06-27 18:56:23 -0400

    replace $.get() with $.ajax, now the servlet can parse the json data sent by client

--- Code/DashBoard/myapp/js/myjs.js
+++ Code/DashBoard/myapp/js/myjs.js
@@ -29,262 +29,134 @@ $(document).ready(function(){
     			selected: selectInstanceJson1
     		},
     		success : function(resp) {
- 			//now for debug, the json is 
- 			//[{"id":"1","performance":[{"date":"1","value":"1"},{"date":"2","value":"2"}]},{"id":"2","performance":[{"date":"1","value":"2"},{"date":"2","value":"1"}]}]
- 			performances = resp;
- 			//console.log(performances.length);
- 			
- 			$('#myModal').on('shown.bs.modal', function(e) {
+	 			//now for debug, the json is 
+	 			//[{"id":"1","performance":[{"date":"1","value":"1"},{"date":"2","value":"2"}]},{"id":"2","performance":[{"date":"1","value":"2"},{"date":"2","value":"1"}]}]
+	 			performances = resp;
+	 			//console.log(performances.length);
+	 			
+	 			$('#myModal').on('shown.bs.modal', function(e) {
 
- 				var datas = new Array();
-				var xValue = new Array();
-				
-				//prefare for the x axis value, xValue and datas will be used to form the parameter of xs
-				for (var i = 0; i < performances.length; i++) {
-					xValue[i] = 'x' + i;
-				}
-				for ( i = 0; i < performances.length; i++) {
-					datas[i] = performances[i].id;
-				}
- 				
- 				//var xsPara = '{' + "\"" + datas[0] + "\": \"x1\",\""+datas[1]+"\": \"x2\"}";
- 				//the parameter for xs, which format is : 		{
-			    // 												'1': 'x0',
-			    // 												'2': 'x1',
-			    // 												},
- 				var xsPara = '{';
- 				for (var i = 0; i < performances.length - 1; i++) {
- 					xsPara = xsPara + "\"" + datas[i] + "\":" + "\"" + xValue[i] + "\",";
- 				}
- 				xsPara = xsPara + "\"" + datas[performances.length - 1] + "\":" + "\"" + xValue[performances.length - 1] + "\"}";
-
-				var ob = JSON.parse(xsPara);
-				//console.log(ob); 
-
-				//the parameter for columns, which format is : [
-		        //      			['x0', performances[0].performance[0].date,performances[0].performance[1].date],
-		        //     				['x1', performances[1].performance[0].date,3],
-				//       			['1', performances[0].performance[0].value,performances[0].performance[1].value],
-				//        			['2', performances[1].performance[0].value,performances[1].performance[1].value]
-				//      		]
-				//when use, we should choose proper value for cpu, mem and disk
-				var memColumn = [];
-				for (var i = 0; i < performances.length; i++) {
-					var item = [xValue[i]];
-					for (var j = 0; j < performances[i].performance.length; j++) {
-						item.push(performances[i].performance[j].date);
+	 				var datas = new Array();
+					var xValue = new Array();
+					
+					//prefare for the x axis value, xValue and datas will be used to form the parameter of xs
+					for (var i = 0; i < performances.length; i++) {
+						xValue[i] = 'x' + i;
 					}
-					memColumn.push(item);
-				}
-				for (var i = 0; i < performances.length; i++) {
-					var item = [datas[i]];
-					for (var j = 0; j < performances[i].performance.length; j++) {
-						item.push(performances[i].performance[j].value);
+					for ( i = 0; i < performances.length; i++) {
+						datas[i] = performances[i].id;
 					}
-					memColumn.push(item);
-				}
-				//console.log(memColumn);
+	 				
+	 				//var xsPara = '{' + "\"" + datas[0] + "\": \"x1\",\""+datas[1]+"\": \"x2\"}";
+	 				//the parameter for xs, which format is : 		{
+				    // 												'1': 'x0',
+				    // 												'2': 'x1',
+				    // 												},
+	 				var xsPara = '{';
+	 				for (var i = 0; i < performances.length - 1; i++) {
+	 					xsPara = xsPara + "\"" + datas[i] + "\":" + "\"" + xValue[i] + "\",";
+	 				}
+	 				xsPara = xsPara + "\"" + datas[performances.length - 1] + "\":" + "\"" + xValue[performances.length - 1] + "\"}";
 
-				//show how one line chart works with json input
-		        var cpuchart = c3.generate({
-				    bindto: '#cpuChart',
-				    data: {
-				    	json: resp[1].performance,
-					    keys:{
-					    	x:'date',
-					    	value: ['value'],
+					var ob = JSON.parse(xsPara);
+					//console.log(ob); 
+
+					//the parameter for columns, which format is : [
+			        //      			['x0', performances[0].performance[0].date,performances[0].performance[1].date],
+			        //     				['x1', performances[1].performance[0].date,3],
+					//       			['1', performances[0].performance[0].value,performances[0].performance[1].value],
+					//        			['2', performances[1].performance[0].value,performances[1].performance[1].value]
+					//      		]
+					//when use, we should choose proper value for cpu, mem and disk
+					var memColumn = [];
+					for (var i = 0; i < performances.length; i++) {
+						var item = [xValue[i]];
+						for (var j = 0; j < performances[i].performance.length; j++) {
+							item.push(performances[i].performance[j].date);
+						}
+						memColumn.push(item);
+					}
+					for (var i = 0; i < performances.length; i++) {
+						var item = [datas[i]];
+						for (var j = 0; j < performances[i].performance.length; j++) {
+							item.push(performances[i].performance[j].value);
+						}
+						memColumn.push(item);
+					}
+					//console.log(memColumn);
+
+					//show how one line chart works with json input
+			        var cpuchart = c3.generate({
+					    bindto: '#cpuChart',
+					    data: {
+					    	json: resp[1].performance,
+						    keys:{
+						    	x:'date',
+						    	value: ['value'],
+						    },
 					    },
-				    },
-				    zoom: {
-				        enabled: true
-				    },
-				    subchart: {
-				        show: true
-				    },
-				    legend: {
-				        position: 'right'
-				    }
-				});
-				
-				//show how multiple lines chart works with different x axis value
-				var memchart = c3.generate({
-				    bindto: '#MemoryChart',
-				    
-				    data: { 
-			          xs: ob,
-				      columns : memColumn
-				    },
-				    zoom: {
-				        enabled: true
-				    },
-				    subchart: {
-				        show: true
-				    },
-				    legend: {
-				        position: 'right'
-				    }
-				});
+					    zoom: {
+					        enabled: true
+					    },
+					    subchart: {
+					        show: true
+					    },
+					    legend: {
+					        position: 'right'
+					    }
+					});
+					
+					//show how multiple lines chart works with different x axis value
+					var memchart = c3.generate({
+					    bindto: '#MemoryChart',
+					    
+					    data: { 
+				          xs: ob,
+					      columns : memColumn
+					    },
+					    zoom: {
+					        enabled: true
+					    },
+					    subchart: {
+					        show: true
+					    },
+					    legend: {
+					        position: 'right'
+					    }
+					});
 
-				//show how the c3.generate() function get parameters from outside
-				var target = "\"#DiskChart\"";
-				var text = '{"bindto":'+target+',"data":{"columns":[["data1",30,200,100,400,150,250]]}}';
-				var obj = JSON.parse(text);
-				var diskchart = c3.generate(obj);
-				// var diskchart = c3.generate({
-				//     bindto: '#DiskChart',
-				//     data: {
-				//         xs: {
-				//             'data[1]': 'x1',
-				//             'data[2]': 'x2',
-				//         },
-				//         columns: [
-				//             ['x1', 10, 30, 45, 50, 70, 100],
-				//             ['x2', 30, 50, 75, 100, 120],
-				//             ['data[1]', 30, 200, 100, 400, 150, 250],
-				//             ['data[2]', 20, 180, 240, 100, 190]
-				//         ]
-				//     },
-				//     zoom: {
-				//         enabled: true
-				//     },
-				//     subchart: {
-				//         show: true
-				//     },
-				//     legend: {
-				//         position: 'right'
-				//     }
-				// });
-			});
+					//show how the c3.generate() function get parameters from outside
+					var target = "\"#DiskChart\"";
+					var text = '{"bindto":'+target+',"data":{"columns":[["data1",30,200,100,400,150,250]]}}';
+					var obj = JSON.parse(text);
+					var diskchart = c3.generate(obj);
+					// var diskchart = c3.generate({
+					//     bindto: '#DiskChart',
+					//     data: {
+					//         xs: {
+					//             'data[1]': 'x1',
+					//             'data[2]': 'x2',
+					//         },
+					//         columns: [
+					//             ['x1', 10, 30, 45, 50, 70, 100],
+					//             ['x2', 30, 50, 75, 100, 120],
+					//             ['data[1]', 30, 200, 100, 400, 150, 250],
+					//             ['data[2]', 20, 180, 240, 100, 190]
+					//         ]
+					//     },
+					//     zoom: {
+					//         enabled: true
+					//     },
+					//     subchart: {
+					//         show: true
+					//     },
+					//     legend: {
+					//         position: 'right'
+					//     }
+					// });
+				});
 			},
     	});
- 	// 	$.get('performance', selectInstanceJson, function(resp) {
- 	// 		//now for debug, the json is 
- 	// 		//[{"id":"1","performance":[{"date":"1","value":"1"},{"date":"2","value":"2"}]},{"id":"2","performance":[{"date":"1","value":"2"},{"date":"2","value":"1"}]}]
- 	// 		performances = resp;
- 	// 		//console.log(performances.length);
- 			
- 	// 		$('#myModal').on('shown.bs.modal', function(e) {
-
- 	// 			var datas = new Array();
-		// 		var xValue = new Array();
-				
-		// 		//prefare for the x axis value, xValue and datas will be used to form the parameter of xs
-		// 		for (var i = 0; i < performances.length; i++) {
-		// 			xValue[i] = 'x' + i;
-		// 		}
-		// 		for ( i = 0; i < performances.length; i++) {
-		// 			datas[i] = performances[i].id;
-		// 		}
- 				
- 	// 			//var xsPara = '{' + "\"" + datas[0] + "\": \"x1\",\""+datas[1]+"\": \"x2\"}";
- 	// 			//the parameter for xs, which format is : 		{
-		// 	    // 												'1': 'x0',
-		// 	    // 												'2': 'x1',
-		// 	    // 												},
- 	// 			var xsPara = '{';
- 	// 			for (var i = 0; i < performances.length - 1; i++) {
- 	// 				xsPara = xsPara + "\"" + datas[i] + "\":" + "\"" + xValue[i] + "\",";
- 	// 			}
- 	// 			xsPara = xsPara + "\"" + datas[performances.length - 1] + "\":" + "\"" + xValue[performances.length - 1] + "\"}";
-
-		// 		var ob = JSON.parse(xsPara);
-		// 		//console.log(ob); 
-
-		// 		//the parameter for columns, which format is : [
-		//         //      			['x0', performances[0].performance[0].date,performances[0].performance[1].date],
-		//         //     				['x1', performances[1].performance[0].date,3],
-		// 		//       			['1', performances[0].performance[0].value,performances[0].performance[1].value],
-		// 		//        			['2', performances[1].performance[0].value,performances[1].performance[1].value]
-		// 		//      		]
-		// 		//when use, we should choose proper value for cpu, mem and disk
-		// 		var memColumn = [];
-		// 		for (var i = 0; i < performances.length; i++) {
-		// 			var item = [xValue[i]];
-		// 			for (var j = 0; j < performances[i].performance.length; j++) {
-		// 				item.push(performances[i].performance[j].date);
-		// 			}
-		// 			memColumn.push(item);
-		// 		}
-		// 		for (var i = 0; i < performances.length; i++) {
-		// 			var item = [datas[i]];
-		// 			for (var j = 0; j < performances[i].performance.length; j++) {
-		// 				item.push(performances[i].performance[j].value);
-		// 			}
-		// 			memColumn.push(item);
-		// 		}
-		// 		//console.log(memColumn);
-
-		// 		//show how one line chart works with json input
-		//         var cpuchart = c3.generate({
-		// 		    bindto: '#cpuChart',
-		// 		    data: {
-		// 		    	json: resp[1].performance,
-		// 			    keys:{
-		// 			    	x:'date',
-		// 			    	value: ['value'],
-		// 			    },
-		// 		    },
-		// 		    zoom: {
-		// 		        enabled: true
-		// 		    },
-		// 		    subchart: {
-		// 		        show: true
-		// 		    },
-		// 		    legend: {
-		// 		        position: 'right'
-		// 		    }
-		// 		});
-				
-		// 		//show how multiple lines chart works with different x axis value
-		// 		var memchart = c3.generate({
-		// 		    bindto: '#MemoryChart',
-				    
-		// 		    data: { 
-		// 	          xs: ob,
-		// 		      columns : memColumn
-		// 		    },
-		// 		    zoom: {
-		// 		        enabled: true
-		// 		    },
-		// 		    subchart: {
-		// 		        show: true
-		// 		    },
-		// 		    legend: {
-		// 		        position: 'right'
-		// 		    }
-		// 		});
-
-		// 		//show how the c3.generate() function get parameters from outside
-		// 		var target = "\"#DiskChart\"";
-		// 		var text = '{"bindto":'+target+',"data":{"columns":[["data1",30,200,100,400,150,250]]}}';
-		// 		var obj = JSON.parse(text);
-		// 		var diskchart = c3.generate(obj);
-		// 		// var diskchart = c3.generate({
-		// 		//     bindto: '#DiskChart',
-		// 		//     data: {
-		// 		//         xs: {
-		// 		//             'data[1]': 'x1',
-		// 		//             'data[2]': 'x2',
-		// 		//         },
-		// 		//         columns: [
-		// 		//             ['x1', 10, 30, 45, 50, 70, 100],
-		// 		//             ['x2', 30, 50, 75, 100, 120],
-		// 		//             ['data[1]', 30, 200, 100, 400, 150, 250],
-		// 		//             ['data[2]', 20, 180, 240, 100, 190]
-		// 		//         ]
-		// 		//     },
-		// 		//     zoom: {
-		// 		//         enabled: true
-		// 		//     },
-		// 		//     subchart: {
-		// 		//         show: true
-		// 		//     },
-		// 		//     legend: {
-		// 		//         position: 'right'
-		// 		//     }
-		// 		// });
-		// 	});
-		// }); 		
     });
 	
 	//
